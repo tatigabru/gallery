9421d859d4e55b0360a2e78088ef1d4678daf75d
tatigabru <travellingtati@gmail.com>
2025-03-05 21:37:07 -0800

    Update loader and error message

--- app/paintings/error.js
+++ app/paintings/error.js
@@ -5,7 +5,7 @@ export default function Error({error}) {
     return (
     <main className='error'>
         <h1>An error occured!</h1>
-        <p>Failed to create meal</p>
+        <p>Failed to load painting</p>
     </main>
   );
 }
